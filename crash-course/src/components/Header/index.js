f1bd31c6bf999ca47521ae16fb0974af4f845051
Rafael Lima <drdverzola@gmail.com>
2021-12-09 08:44:33 +0000

    created about

--- crash-course/src/components/Header/index.js
+++ crash-course/src/components/Header/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Link from "next/Link";
 
 function Header() {
     return (
@@ -20,7 +21,9 @@ function Header() {
                     <span className='ml-3 text-xl'>Tailblocks</span>
                 </a>
                 <nav className='md:mr-auto md:ml-4 md:py-1 md:pl-4 md:border-l md:border-gray-400	flex flex-wrap items-center text-base justify-center'>
-                    <a className='mr-5 hover:text-gray-900'>First Link</a>
+                    <Link href='/about'>
+                        <a className='mr-5 hover:text-gray-900'>About</a>
+                    </Link>
                     <a className='mr-5 hover:text-gray-900'>Second Link</a>
                     <a className='mr-5 hover:text-gray-900'>Third Link</a>
                     <a className='mr-5 hover:text-gray-900'>Fourth Link</a>
